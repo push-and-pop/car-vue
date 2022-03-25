0623860f2708a74f010c368330f45350925a0847
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-03-25 15:31:29 +0800

    refactor

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
 
-const service = axios.create({
+const http = axios.create({
     // process.env.NODE_ENV === 'development' 来判断是否开发环境
     // easy-mock服务挂了，暂时不使用了
     // baseURL: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
     timeout: 5000
 });
 
-service.interceptors.request.use(
+http.interceptors.request.use(
     config => {
+        if (config.method == "POST") {
+            //todo
+        }
         return config;
     },
     error => {
@@ -17,7 +20,7 @@ service.interceptors.request.use(
     }
 );
 
-service.interceptors.response.use(
+http.interceptors.response.use(
     response => {
         if (response.status === 200) {
             return response.data;
@@ -31,4 +34,4 @@ service.interceptors.response.use(
     }
 );
 
-export default service;
+export default http;
