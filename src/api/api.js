694d9c50282469cd88f2817de774bbb939ea1282
zhaha <80676432+push-and-pop@users.noreply.github.com>
2022-04-19 20:56:29 +0800

    zzz

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,7 @@
 import { get, post } from './request.js'
 
 export const postLogin = data => post("/api/login", data)
+
+export const fetchData = data => get("./table.json", data)
+
+export const postPubAnnouncement = data => post("/api/annocement/creat", data)
--- src/views/announcement.vue
+++ src/views/announcement.vue
@@ -1,27 +1,76 @@
 <template>
-    <div>
-        <el-row>
-            <el-col :span="12" :offset="6">
-                <el-input
-                    class="inputarea"
-                    v-model="textarea"
-                    :autosize="{ minRows: 8, maxRows: 8 }"
-                    type="textarea"
-                    placeholder="输入新公告"
-                />
-            </el-col>
-        </el-row>
-    </div>
+  <div>
+    <el-space wrap fill style="width: 100%" direction="vertical">
+      <el-input
+        v-model="textarea"
+        :autosize="{ minRows: 8, maxRows: 8 }"
+        class="inputarea"
+        type="textarea"
+        placeholder="输入新公告"
+      />
+      <el-button type="primary" @click="pubAnnouncement()">发布</el-button>
+      <el-table :data="tableData" style="width: 100%" height="250">
+        <el-table-column fixed prop="date" label="Date" width="150" />
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column prop="state" label="State" width="120" />
+        <el-table-column prop="city" label="City" width="320" />
+        <el-table-column prop="address" label="Address" width="600" />
+        <el-table-column prop="zip" label="Zip" width="120" />
+      </el-table>
+      <el-pagination
+        :page-size="20"
+        :pager-count="5"
+        layout="prev, pager, next"
+        :total="1000"
+      />
+    </el-space>
+  </div>
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
-const textarea = ref("")
+import { postPubAnnouncement } from "../api/api";
+const textarea = ref("");
+const pubAnnouncement = () => {
+  if (textarea.value === "") {
+    return;
+  }
+  postPubAnnouncement({
+    msg: textarea.value,
+  })
+    .then((res) => {
+      let data = res.data.data;
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log(e);
+      ElMessage.error("登录失败");
+    });
+};
 </script>
 
 <style  scoped>
 .inputarea {
-    height: 70px;
-    text-align: center;
+  width: 800px;
+}
+
+.el-row {
+  height: 170px;
+  display: flex;
+}
+.el-row2 {
+  height: 60px;
+  display: flex;
+}
+.input {
+  height: 30%;
+  width: 100%;
+}
+.table {
+  height: 70%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
